19346f64a21de5a5567f3fa3b5a0e85bf98ac4a6
马飞扬 <2594438925@qq.com>
2025-10-23 17:40:55 +0800

    change to launch

--- startup.sh
+++ startup.sh
@@ -2,5 +2,4 @@
 export ROS_LOG_DIR=/home/horsefly/下载/Binocular_ros2/logs
 # source /opt/ros/<ros2-distro>/setup.bash
 source /home/horsefly/下载/Binocular_ros2/install/setup.bash
-chmod 666 /dev/video0
-ros2 run pub pub
+ros2 launch pub pub_launch.py
